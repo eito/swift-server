dd230a68e165cc373885920456d2f9498eebe869
Eric Ito <eric.ito@gmail.com>
2016-05-07 23:07:21 -0700

    Initial Commit

---
+++ swift-server/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-server
+//
+//  Created by Eric Ito on 5/7/16.
+//  Copyright © 2016 Eric Ito. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
